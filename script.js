cc2dd91258ada8124e9b63a51fd129c77f2d8ca9
Dima163163 <khlopovdima@gmail.com>
2023-04-27 19:47:43 +0400

    lessonhard07

--- script.js
+++ script.js
@@ -16,18 +16,11 @@ let newArr = [];
 for (let i = 0; i < arr.length; i++) {
   newArr = "<br />" + arr[i] + "<br />";
   document.write(newArr);
-}
-
-for (let i = 0; i < arr.length; i++) {
   if (arr[i] == "Суббота" || arr[i] == "Воскресенье") {
     document.write("<br /><i>" + arr[i] + "</i><br />");
   }
-}
-
-const day = new Date();
-const todayDay = day.getDay() - 1;
-
-for (let i = 0; i < arr.length; i++) {
+  const day = new Date();
+  const todayDay = day.getDay() - 1;
   if (i == todayDay) {
     document.write("<br /><b>" + arr[i] + "</b><br />");
   }
